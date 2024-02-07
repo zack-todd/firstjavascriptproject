8e2b888b9fae0dc626ffcd61972da68906d69873
Nick Todd <nick.todd@conygre.com>
2024-02-07 21:29:34 +0000

    some java script

--- Index.js
+++ Index.js
@@ -1,34 +1,15 @@
-console.log("hello");
+console.log;
 
-// these are variables
 let firstName = "Zack";
-let lastName = "Todd";
+
 let age = "20";
 
-// this is a double
-let height =  6.1;
-// prints to the screen
-console.log(firstName, lastName, age);
+let height = "5'10";
 
-/* this is a multi line comment 
-it can sopam multiple lines
-and is sued for longer comments */
-
-// single line comment
-
-console.log(7*7);
-
-console.log(5/2);
-
-console.log(6+2);
-
-console.log(300-59);
-
-console.log("My Name is" + firstName + " " + lastName + "and I am" + age + "years old.")
-console.log("My height is" + height + "inches.")
-
-console.log(`my name is  ${firstName} ${lastName} and I am ${age} years old`);
-
-console.log(age>50)
-let favoriteNumber = 10;
-console.log(favoriteNumber==="10")
+console.log(firstName);
+console.log(age);
+console.log(height);
+console.log(firstName, age, height);
+console.log(firstName + " " + age + " " + height);
+console.log(`${firstName} ${age} ${height}`);
+console.log(firstName + " is " + age + " years old");
